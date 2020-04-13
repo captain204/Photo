c7d36203cc1bbf1cd5db821cfe3191b94604b7f5
captain204 <nurudeenakindele8@gmail.com>
2020-04-13 20:49:55 +0100

    heroku

--- config.py
+++ config.py
@@ -2,9 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-#SQLALCHEMY_DATABASE_URI ="postgresql:///photodb"
-from flask_heroku import Heroku
-heroku = Heroku(app) 
+#SQLALCHEMY_DATABASE_URI ="postgresql:///photodb" 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 
--- photo/views.py
+++ photo/views.py
@@ -8,6 +8,7 @@ from flask_httpauth import HTTPBasicAuth
 from flask import g
 
 
+
 auth = HTTPBasicAuth()
 
 
