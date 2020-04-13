28a5cce00345d65969b7ba3569fb1e688f4a3b30
captain204 <nurudeenakindele8@gmail.com>
2020-04-13 13:10:46 +0100

    app modified

--- photo/app.py
+++ photo/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from models import db
-from views import photo_blueprint
+from photo.models import db
+from photo.views import photo_blueprint
 
 
 def create_app(config_filename):
