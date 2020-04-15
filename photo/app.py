afa0a296a1ee37462f7360ab18be5dd54cdcec6f
captain204 <nurudeenakindele8@gmail.com>
2020-04-15 17:45:30 +0100

    docs

--- photo/app.py
+++ photo/app.py
@@ -5,30 +5,6 @@ from flask_migrate import Migrate
 from photo.models import db
 from photo.views import photo_blueprint
 from flasgger import Swagger
-
-"""template = {
-  "swagger": "2.0",
-  "info": {
-    "title": "Photo API",
-    "description": "API for saving links to favourite user photos",
-    "contact": {
-      "responsibleOrganization": "ME",
-      "responsibleDeveloper": "Me",
-      "email": "nurudeenakindele8@gmail.com",
-      "url": "www.me.com",
-    },
-    "termsOfService": "http://me.com/terms",
-    "version": "0.0.1"
-  },
-  "host": "favouritephotoapi.herokuapp.com",  # overrides localhost:500
-  "basePath": "/photo/",  # base bash for blueprint registration
-  "schemes": [
-    "http",
-    "https"
-  ],
-  "operationId": "getmyData"
-}"""
-
 template= {
     "swagger": "3.0",
     "openapi": "3.0.0",
@@ -75,7 +51,7 @@ template= {
       }
     }
   }
-)
+
 
 def create_app(config_filename):
     app = Flask(__name__, instance_relative_config=True)
